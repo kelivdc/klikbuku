c175750c35cdd4a24a8e3a2921485e536577d069
Arie <kelivdc@yahoo.com>
2023-06-23 21:21:37 +0700

    Ini

---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+function About() {
+  return (
+    <>
+    [<Link href="/">Home</Link>]
+    <div>About</div>
+    </>
+  )
+}
+
+export default About
---
+++ app/cms/login/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='flex items-center justify-center h-screen border-solid'>
+      LOGIN 
+    </div>
+  )
+}
+
+export default Login
